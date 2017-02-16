dc8568d0b9bc482bf1252aeb0f34e1f0c7bbea39
amm0924 <anurag.mishra@springernature.com>
2017-02-16 12:05:05 +0530

    first commit

---
+++ src/com/kanchan/FirstGit.java
@@ -0,0 +1,10 @@
+package com.kanchan;
+
+/**
+ * Created by amm0924 on 2/16/17.
+ */
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("welcome to git");
+    }
+}
