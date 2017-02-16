b6d2baac4b0bbb8381a1c33a77b5d39e8231c98f
Kanchan Mishra <kmishra@kennovation-services.com>
2017-02-16 12:46:56 +0530

    my commit message

---
+++ src/com/kanchan/Addition.java
@@ -0,0 +1,12 @@
+package com.kanchan;
+
+/**
+ * Created by infraadmin on 16/2/17.
+ */
+public class Addition {
+    public static void main(String[] args) {
+        int a = 10,b = 20,c;
+        c = a + b;
+        System.out.println("Addition is : " + c);
+    }
+}
